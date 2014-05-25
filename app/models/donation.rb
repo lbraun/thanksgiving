0821368e6be137a8fe03aa8e8144b249d6ea548a
Lucas Braun <lucasalan@gmail.com>
2014-05-25 12:31:43 -0400

    Updated donation method selection

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -3,12 +3,17 @@ class Donation < ActiveRecord::Base
 
   validates_presence_of :amount, :recipient
 
-  DONATION_METHODS = %w{
+  VALID_METHODS = %w{
     credit_card
     cash
+    other
   }
 
   def to_s
     "$#{amount} to #{recipient.name}"
+  end
+
+  def self.method_select_options
+    VALID_METHODS.map { |method| [method.humanize, method] }
   end
 end
