d180ddccdc2986ba79f0f4264699a62cc9d43321
Lucas Braun <lucas.braun@annkissam.com>
2015-05-09 19:04:41 -0400

    Added authentication for production too

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,12 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '751496528958-v4kr77fuktu3eq0mb2trvlofr128ramg.apps.googleusercontent.com', 'f3AryFrylkpCLRx5rBIJRe5W', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  if Rails.env.production?
+    client_id = '751496528958-7c3pji7tqjmk04stbadth7i7tvgi610t.apps.googleusercontent.com'
+    client_secret = 'JgUnFJVDGI_OsupoKQWj_0kI'
+  else
+    client_id = '751496528958-v4kr77fuktu3eq0mb2trvlofr128ramg.apps.googleusercontent.com'
+    client_secret = 'f3AryFrylkpCLRx5rBIJRe5W'
+  end
+  provider :google_oauth2, client_id, client_secret, {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
