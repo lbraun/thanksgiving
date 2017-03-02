cfcc469fa422247f1c38b9631418589c4cd823ac
Brad <bgreen3@uoregon.edu>
2017-03-01 21:37:19 -0700

    Remove hash rockets in favor of newer syntax

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -13,21 +13,21 @@ feature "user management" do
     end
 
     it "sees a table with proper headers and admin row data" do
-      page.find("#user_#{admin.id} td:nth-of-type(1)", :text => admin.name)
-      page.find("#user_#{admin.id} td:nth-of-type(2)", :text => admin.created_at_string)
-      page.find("#user_#{admin.id} td:nth-of-type(3)", :text => "Yes")
-      page.find("#user_#{admin.id} td:nth-of-type(4)", :text => admin.approval_at_string)
-      page.find("#user_#{admin.id} td:nth-of-type(5)", :text => "Yes")
-      page.find("#user_#{admin.id} td:nth-of-type(6)", :text => "Delete")
+      page.find("#user_#{admin.id} td:nth-of-type(1)", text: admin.name)
+      page.find("#user_#{admin.id} td:nth-of-type(2)", text: admin.created_at_string)
+      page.find("#user_#{admin.id} td:nth-of-type(3)", text: "Yes")
+      page.find("#user_#{admin.id} td:nth-of-type(4)", text: admin.approval_at_string)
+      page.find("#user_#{admin.id} td:nth-of-type(5)", text: "Yes")
+      page.find("#user_#{admin.id} td:nth-of-type(6)", text: "Delete")
     end
 
     it "sees a table with proper headers and user row data" do
-      page.find("#user_#{users.first.id} td:nth-of-type(1)", :text => users.first.name)
-      page.find("#user_#{users.first.id} td:nth-of-type(2)", :text => users.first.created_at_string)
-      page.find("#user_#{users.first.id} td:nth-of-type(3)", :text => "Yes")
-      page.find("#user_#{users.first.id} td:nth-of-type(4)", :text => users.first.approval_at_string)
-      page.find("#user_#{users.first.id} td:nth-of-type(5)", :text => "No")
-      page.find("#user_#{users.first.id} td:nth-of-type(6)", :text => "Delete")
+      page.find("#user_#{users.first.id} td:nth-of-type(1)", text: users.first.name)
+      page.find("#user_#{users.first.id} td:nth-of-type(2)", text: users.first.created_at_string)
+      page.find("#user_#{users.first.id} td:nth-of-type(3)", text: "Yes")
+      page.find("#user_#{users.first.id} td:nth-of-type(4)", text: users.first.approval_at_string)
+      page.find("#user_#{users.first.id} td:nth-of-type(5)", text: "No")
+      page.find("#user_#{users.first.id} td:nth-of-type(6)", text: "Delete")
     end
 
     context "and admin clicks No for user approval" do
@@ -45,7 +45,7 @@ feature "user management" do
       end
 
       it "shows Yes instead of No button" do
-        expect(page).to have_link('Yes', :href => remove_approval_user_path(unapproved_user))
+        expect(page).to have_link('Yes', href: remove_approval_user_path(unapproved_user))
       end
 
       context "and the admin clicks No for user approval for same user" do
