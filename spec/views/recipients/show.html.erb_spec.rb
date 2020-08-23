84cb5b6b7c4b2ca8a005df60d47ed6ce46088e56
Will Harris-Braun <willharrisbraun@gmail.com>
2020-08-23 19:33:44 -0400

    Update recipients show page to use strings from en.yml

---
+++ spec/views/recipients/show.html.erb_spec.rb
@@ -0,0 +1,19 @@
+describe 'recipients/show' do
+  it 'displays a recipient\'s attrubitues' do
+    assign(:recipient,
+      Recipient.new(
+        id: 1,
+        name: 'School',
+        url: 'https://www.example.com',
+        reasons_for_support: 'It\'s good',
+      )
+    )
+    assign(:donations, [])
+
+    render
+
+    expect(rendered).to match /Reasons for support:/
+    expect(rendered).to match /URL:/
+    expect(rendered).to match /My giving history:/
+  end
+end
