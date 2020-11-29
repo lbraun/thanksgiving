96fa7b5e55592b37b775f87d8f746bba0ea09959
Lucas Braun <lbraun@greenriver.com>
2020-11-29 18:28:56 +0100

    Add templates for header and footer in typeahead suggestion box

--- app/assets/javascripts/donations.js
+++ app/assets/javascripts/donations.js
@@ -10,10 +10,37 @@ var recipients = new Bloodhound({
 // Initialize the bloodhound suggestion engine
 recipients.initialize();
 
+function makeAddNewLink({query}) {
+  return `<i><a class="tt-suggestion" href="/recipients/new">Add new recipient "${query}"</a></i>`
+}
+
+function makeFooter({query}) {
+  return '<i><a class="tt-suggestion" target="_blank" href="/recipients">See all recipients ></a></i>'
+}
+
+function makeNotFound(object) {
+  return [
+    makeAddNewLink(object),
+    makeFooter(object),
+  ].join("\n")
+}
+
+function makeHeader({query, suggestions}) {
+  if (!suggestions.some((s) => s.name.toUpperCase() === query.toUpperCase())) {
+    return makeAddNewLink({query})
+  }
+}
+
 // Instantiate the typeahead UI
 function instantiateRecipientsTypeahead() {
   $('.typeahead').typeahead(null, {
     displayKey: 'name',
-    source: recipients.ttAdapter()
+    source: recipients.ttAdapter(),
+    templates: {
+      header: makeHeader,
+      notFound: makeNotFound,
+      footer: makeFooter,
+    }
   })
 }
+
